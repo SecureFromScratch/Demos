af1873bae3313c985721d5e757f23f1231456f7d
securefromscratch <securefromscratch@gmail.com>
2025-12-02 14:18:12 +0200

    argon

--- java/spring_with_security/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ java/spring_with_security/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,15 +1,15 @@
 package com.example.demo.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -19,23 +19,40 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/", "/home", "/register", "/setup").permitAll()
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-            )
-            .formLogin((form) -> form
-                .loginPage("/login")
-                .defaultSuccessUrl("/hello", true)
-                .permitAll()
-            )
-            .logout((logout) -> logout.permitAll());
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home", "/register", "/setup").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/hello", true)
+                        .permitAll())
+                .logout((logout) -> logout.permitAll());
 
         return http.build();
     }
+    
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        // Reasonable defaults for web apps
+        int saltLength = 16; // bytes
+        int hashLength = 32; // bytes
+        int parallelism = 1; // currently 1 thread
+        int memory = 1 << 16; // 64 MB
+        int iterations = 3;
+
+        Argon2PasswordEncoder argon2 = new Argon2PasswordEncoder(
+                saltLength,
+                hashLength,
+                parallelism,
+                memory,
+                iterations);
+
+        String idForEncode = "argon2";
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put(idForEncode, argon2);
+
+        return new DelegatingPasswordEncoder(idForEncode, encoders);
     }
 }
